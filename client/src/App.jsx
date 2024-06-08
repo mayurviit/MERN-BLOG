6d23aee7e562690d446a8ec41142752f01649caf
mayurviit <mayurshete1505@gmail.com>
2024-06-08 12:10:01 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,12 @@ import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import Signin from "./pages/Signin"
 import SignUp from "./pages/SignUp"
+import Header from './components/Header'
+
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
