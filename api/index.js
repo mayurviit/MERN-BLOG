251c1eeb9a449218be944226b91653cc1827a856
mayurviit <mayurshete1505@gmail.com>
2024-06-08 13:24:30 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
-
+const mongoose =require("mongoose")
 const app=express();
+const dotenv=require("dotenv")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO) 
+        .then(() =>{ console.log("DB Connected Successfully")})
+        .catch( (error) => {
+//         console.log("DB Connection Failed");
+          console.error(error);
+//         process.exit(1);
+    } )
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000')
 })
+
+
+
