602eae93ca646f03cc95ba3d69456cfbd2ed7924
mayurviit <mayurshete1505@gmail.com>
2024-06-20 22:57:47 +0530

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -54,17 +54,17 @@ exports.updateUser = async (req, res, next) => {
   }
 };
 
-// exports.deleteUser = async (req, res, next) => {
-//   if (!req.user.isAdmin && req.user.id !== req.params.userId) {
-//     return next(errorHandler(403, 'You are not allowed to delete this user'));
-//   }
-//   try {
-//     await User.findByIdAndDelete(req.params.userId);
-//     res.status(200).json('User has been deleted');
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+exports.deleteUser = async (req, res, next) => {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
 
 // export const signout = (req, res, next) => {
 //   try {
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,8 +5,9 @@ const express=require('express');
 const {test} =require('../controllers/user.controller');
 const {updateUser}=require('../controllers/user.controller')
 const {verifyToken} =require('../utils/verifyUser')
-
+const {deleteUser}=require("../controllers/user.controller")
 const router=express.Router();
 router.get('/test',test);
 router.put('/update/:userId',verifyToken,updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
 module.exports = router
