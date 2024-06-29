5be8f212bcd8199fcf7bb260ebf1f65bbc519950
mayurviit <mayurshete1505@gmail.com>
2024-06-29 18:37:20 +0530

    create get post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 const express =require('express');
 const { verifyToken } = require('../utils/verifyUser');
-const { create } = require('../controllers/post.controller');
+const { create, getposts } = require('../controllers/post.controller');
 
 
 
 const router =express.Router();
 
 router.post('/create',verifyToken,create);
+router.get('/getposts',getposts);
 module.exports = router;
