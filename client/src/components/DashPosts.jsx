dd8db786a664f01d481bc5b782f7938cf850cc94
mayurviit <mayurshete1505@gmail.com>
2024-07-01 19:14:30 +0530

    show user posts inside dashboards

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { set } from 'mongoose';
 
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
@@ -10,10 +11,7 @@ export default function DashPosts() {
   const [showMore, setShowMore] = useState(true);
   const [showModal, setShowModal] = useState(false);
   const [postIdToDelete, setPostIdToDelete] = useState('');
-
   useEffect(() => {
-    if (!currentUser) return;
-
     const fetchPosts = async () => {
       try {
         const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
@@ -28,11 +26,10 @@ export default function DashPosts() {
         console.log(error.message);
       }
     };
-
     if (currentUser.isAdmin) {
       fetchPosts();
     }
-  }, [currentUser]);
+  }, [currentUser._id]);
 
   const handleShowMore = async () => {
     const startIndex = userPosts.length;
@@ -75,25 +72,27 @@ export default function DashPosts() {
   };
 
   return (
-    <div className=' overflow-x-auto -mx-12 -my-12 '>
-      {currentUser?.isAdmin && userPosts.length > 0 ? (
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 -my-11 '>
+      {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
-          <Table className='min-w-full bg-white dark:bg-gray-800 shadow-md my-20 '>
-            <Table.Head className='bg-gray-200 dark:bg-gray-700'>
-              <Table.HeadCell className='px-4 py-2'>Date updated</Table.HeadCell>
-              <Table.HeadCell className='px-4 py-2'>Post image</Table.HeadCell>
-              <Table.HeadCell className='px-4 py-2'>Post title</Table.HeadCell>
-              <Table.HeadCell className='px-4 py-2'>Category</Table.HeadCell>
-              <Table.HeadCell className='px-4 py-2'>Delete</Table.HeadCell>
-              <Table.HeadCell className='px-4 py-2'>Edit</Table.HeadCell>
+          <Table hoverable className=' my-20  bg-transparent '>
+            <Table.Head>
+              <Table.HeadCell>Date updated</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
             </Table.Head>
             {userPosts.map((post) => (
-              <Table.Body key={post._id} className='divide-y'>
-                <Table.Row className='hover:bg-gray-100 dark:hover:bg-gray-700'>
-                  <Table.Cell className='px-4 py-2'>
+              <Table.Body className='divide-y'>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                  <Table.Cell>
                     {new Date(post.updatedAt).toLocaleDateString()}
                   </Table.Cell>
-                  <Table.Cell className='px-4 py-2'>
+                  <Table.Cell>
                     <Link to={`/post/${post.slug}`}>
                       <img
                         src={post.image}
@@ -102,7 +101,7 @@ export default function DashPosts() {
                       />
                     </Link>
                   </Table.Cell>
-                  <Table.Cell className='px-4 py-2'>
+                  <Table.Cell>
                     <Link
                       className='font-medium text-gray-900 dark:text-white'
                       to={`/post/${post.slug}`}
@@ -110,8 +109,8 @@ export default function DashPosts() {
                       {post.title}
                     </Link>
                   </Table.Cell>
-                  <Table.Cell className='px-4 py-2'>{post.category}</Table.Cell>
-                  <Table.Cell className='px-4 py-2'>
+                  <Table.Cell>{post.category}</Table.Cell>
+                  <Table.Cell>
                     <span
                       onClick={() => {
                         setShowModal(true);
@@ -122,12 +121,12 @@ export default function DashPosts() {
                       Delete
                     </span>
                   </Table.Cell>
-                  <Table.Cell className='px-4 py-2'>
+                  <Table.Cell>
                     <Link
                       className='text-teal-500 hover:underline'
                       to={`/update-post/${post._id}`}
                     >
-                      Edit
+                      <span>Edit</span>
                     </Link>
                   </Table.Cell>
                 </Table.Row>
@@ -137,16 +136,14 @@ export default function DashPosts() {
           {showMore && (
             <button
               onClick={handleShowMore}
-              className='w-full text-teal-500 self-center text-sm py-4 mt-4'
+              className='w-full text-teal-500 self-center text-sm py-7'
             >
               Show more
             </button>
           )}
         </>
       ) : (
-        <p className='text-center text-gray-500 dark:text-gray-400'>
-          You have no posts yet!
-        </p>
+        <p>You have no posts yet!</p>
       )}
       <Modal
         show={showModal}
